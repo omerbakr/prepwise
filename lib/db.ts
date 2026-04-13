6e585002e0acb61fef2be2e1175f33329718df3d
omerbakr <y.omerbakir@gmail.com>
2026-04-13 15:28:34 +0300

    chore: use absolute import and validate DATABASE_URL

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,12 @@
 import "dotenv/config";
-import { PrismaClient } from "./generated/prisma";
 import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "@/lib/generated/prisma";
 
-const adapter = new PrismaNeon({
-  connectionString: process.env.DATABASE_URL!,
-});
+const connectionString = process.env.DATABASE_URL;
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not set");
+}
+
+const adapter = new PrismaNeon({ connectionString });
 
 export const prisma = new PrismaClient({ adapter });
